5c10e804027af920df8dcbe7bc2ae3c1c11123d0
Noah Barsky <noahbarsky1@gmail.com>
2020-10-24 22:17:30 -0400

    bluetooth library added

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Routes from './views/Routes'
-import MyLocation from './views/MyLocation';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Routes from "./views/Routes";
+import MyLocation from "./views/MyLocation";
 
-export default function App() {
-  return (
-    <Routes/>
-  );
+export default class App extends React.Component {
+	componentDidMount() {
+		// Load Bluetooth Settings Here
+	}
+
+	render() {
+		return <Routes />;
+	}
 }
 
 const styles = StyleSheet.create({
-  container: { 
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
